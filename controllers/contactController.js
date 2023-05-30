d3433670181c1f6a1d2456e0e862fc56aa94cd54
dd44it <12nelson13@gmail.com>
2023-05-30 16:21:19 +0300

    feat: save routes

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -3,16 +3,16 @@ const Contact = require("../models/contactModel");
 
 // @desc Get all contact
 // @route GET /api/contacts
-// @access public
+// @access private
 
 const getContacts = asyncHandler(async (req, res) => {
-  const contacts = await Contact.find();
+  const contacts = await Contact.find({ user_id: req.user.id });
   res.status(200).json(contacts);
 });
 
 // @desc Create new contact
 // @route POST /api/contacts
-// @access public
+// @access private
 
 const createContact = asyncHandler(async (req, res) => {
   console.log("The request body is:", req.body);
@@ -21,13 +21,13 @@ const createContact = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("All fields are mandatory");
   }
-  const contact = await Contact.create({ name, email, phone });
+  const contact = await Contact.create({ name, email, phone, user_id: req.user.id });
   res.status(201).json(contact);
 });
 
 // @desc Get contact
 // @route GET /api/contacts/:id
-// @access public
+// @access private
 
 const getContact = asyncHandler(async (req, res) => {
   const contact = await Contact.findById(req.params.id);
@@ -40,7 +40,7 @@ const getContact = asyncHandler(async (req, res) => {
 
 // @desc Update contact
 // @route PUT /api/contacts/:id
-// @access public
+// @access private
 
 const updateContact = asyncHandler(async (req, res) => {
   const contact = await Contact.findById(req.params.id);
@@ -58,7 +58,7 @@ const updateContact = asyncHandler(async (req, res) => {
 
 // @desc Delete contact
 // @route DELETE /api/contacts/:id
-// @access public
+// @access private
 
 const deleteContact = asyncHandler(async (req, res) => {
   const contact = await Contact.findById(req.params.id);
