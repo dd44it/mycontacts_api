a3b775a25713393fe1280a33e32e9936d73c2259
dd44it <12nelson13@gmail.com>
2023-05-31 15:09:32 +0300

    feat: add permission to update contacts

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -21,7 +21,12 @@ const createContact = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("All fields are mandatory");
   }
-  const contact = await Contact.create({ name, email, phone, user_id: req.user.id });
+  const contact = await Contact.create({
+    name,
+    email,
+    phone,
+    user_id: req.user.id,
+  });
   res.status(201).json(contact);
 });
 
@@ -48,6 +53,12 @@ const updateContact = asyncHandler(async (req, res) => {
     res.status(404);
     throw new Error("Contact not found");
   }
+
+  if (contact.user_id.toString() !== req.user.id) {
+    res.status(403);
+    throw new Error("User don't have permission to update other user contacts");
+  }
+
   const updatedContact = await Contact.findByIdAndUpdate(
     req.params.id,
     req.body,
@@ -66,7 +77,13 @@ const deleteContact = asyncHandler(async (req, res) => {
     res.status(404);
     throw new Error("Contact not found");
   }
-  await Contact.findByIdAndRemove(req.params.id);
+
+  if (contact.user_id.toString() !== req.user.id) {
+    res.status(403);
+    throw new Error("User don't have permission to update other user contacts");
+  }
+
+  await Contact.deleteOne({ _id: req.params.id });
   const contacts = await Contact.find();
   res.status(200).json({ message: "Contact deleted", contacts });
 });
